382a171363b6c678f2aeba09ec33db0b7efc5b85
Alasdair McIntyre <alasdair.mcintyre@gmail.com>
2023-05-25 00:57:57 +0100

    fix sum of squares program

--- programs/sum_sq.asm
+++ programs/sum_sq.asm
@@ -1,19 +1,22 @@
-;; Compute sum of squares
+;; Compute sum of squares in r0, r1: result in r3
 ;;
-;; 0x30 * 0x30 + 0x40 * 0x40 = 0x1900
+;; 0x52**2 + 0xe7**2 = 0xeab5
 ;;
-        mov r0, 0x30
-        mov r1, 0x30
+        mov r0, 0x52
+        mov r1, 0xe7
+
+        mov r4, r1  ; save r1
+        mov r1, r0
         bl  .mul
         mov r3, r2
-        mov r0, 0x40
-        mov r1, 0x40
+        mov r0, r4  ; restore
+        mov r1, r0
         bl  .mul
         add r3, r2
         hlt
 
 .mul    mov r2, 0
-.loop   tst r0, bit 15
+.loop   tst r0, bit 0
         beq .skip
         add r2, r1
 .skip   lsl r1, 1
