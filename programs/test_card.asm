c730583937e88852adb1712ae0d84bfe285c8a43
Alasdair McIntyre <alasdair.mcintyre@gmail.com>
2023-07-05 18:44:30 +0100

    asm.pl: scoped registers+labels; use them in video test card

--- programs/test_card.asm
+++ programs/test_card.asm
@@ -26,16 +26,12 @@ def TEXT_COLS 64
 def TEXT_ROWS 40
 def TEXT_BASE .MEM_TOP - .TEXT_COLS*.TEXT_ROWS
 
+def GFX_BASE .MEM_TOP - 512*400/8
+
 ; well below video for safety
 def STACK_BASE 0x1000-2
 
-alias r1  ch
-alias r2  n
-alias r3  x
-alias r4  y
-alias r5  dx
-alias r6  dy
-alias r10 video
+;; global aliases
 alias r12 tmp
 alias r13 sp
 alias r14 link
@@ -46,35 +42,62 @@ alias r15 pc
     mov sp, #hi(.STACK_BASE)
     add sp, #lo(.STACK_BASE)
 
-   ;bra .text_test_card   ; display text test-card
-    bra .gfx_test_card2   ; display graphics test-card
+   ;bl .text_test_card   ; display text test-card
+    bl .gfx_test_card2   ; display graphics test-card
 
-.text_test_card
+    hlt
+
+.text_test_card {
+    stw link, [sp]
+    sub sp, #2
+
     ; set 64x40 text
     mov r0, #0
     bl .video_set_mode
-    mov video, #hi(.TEXT_BASE)
-    add video, #lo(.TEXT_BASE)
-    mov ch, #0xc0
+
+    mov r0, #0xc0
     bl .text_clear
     bl .text_card_table
     bl .text_card_ascii
     bl .text_card_rows
     bl .text_card_cols
-    hlt
 
-.gfx_test_card1
+    add sp, #2
+    ldw pc, [sp]
+}
+
+.gfx_test_card1 {
+    alias r0 color
+    alias r1 x
+    alias r2 y
+    alias r3 count
+    alias r4 video
+    alias r5 dx
+    alias r6 dy
+
+    stw link, [sp]
+    sub sp, #8
+    stw r4, [sp,#2]
+    stw r5, [sp,#4]
+    stw r6, [sp,#6]
+
     ; set 512x400 1bpp
     mov r0, #1
     bl .video_set_mode
     bl .gfx_clear
-    mov ch, #0xf000     ; mode1: 8080; mode2: c0c0; mode3: f0f0
-    add ch, #0x00f0
+
+    mov color, #0xf000     ; mode1: 8080; mode2: c0c0; mode3: f0f0
+    add color, #0x00f0
     mov x, #0
     mov y, #0
-    mov n, #hi(200)     ; mode1: 400; mode2: 200; mode3: 200
-    add n, #lo(200)     ; mode1: 400; mode2: 200; mode3: 200
-.gfx_loop
+    mov count, #hi(200)    ; mode1: 400; mode2: 200; mode3: 200
+    add count, #lo(200)    ; mode1: 400; mode2: 200; mode3: 200
+
+    mov video, #hi(.GFX_BASE)
+    add video, #lo(.GFX_BASE)
+
+.loop
+    ; clumsily plot a point
     mov tmp, video
     mov dy, y
     lsl dy, #7          ; mode1: 6; mode2: 6; mode3: 7
@@ -82,90 +105,114 @@ alias r15 pc
     mov dx, x
     lsr dx, #1          ; mode1: 3; mode2: 2; mode3: 1
     add tmp, dx
+    stb color, [tmp]
 
-    stb ch, [tmp]
-    ror ch, #4          ; mode2: 1; mode3: 2; mode4: 4
+    ror color, #4          ; mode2: 1; mode3: 2; mode4: 4
     add x, #1
     add y, #1
-    sub n, #1
+    sub count, #1
     prne
-    bra .gfx_loop
-    hlt
+    bra .loop
 
-.gfx_test_card2
+    ldw r4, [sp,#2]
+    ldw r5, [sp,#4]
+    ldw r6, [sp,#6]
+    add sp, #8
+    ldw pc, [sp]
+}
+
+.gfx_test_card2 {
+    def centre_x 255
+    def centre_y 200
+
+    alias r4 px
+    alias r5 py
+    alias r6 count
+
+    stw link, [sp]
+    sub sp, #8
+    stw r4, [sp,#2]
+    stw r5, [sp,#4]
+    stw r6, [sp,#6]
+
     ; set 512x400 1bpp
     mov r0, #1
     bl .video_set_mode
     bl .gfx_clear
 
-    mov r4, #20<<8   ; px
-    mov r5, #0       ; py
-    mov r6, #50      ; n
+    mov px, #20<<8
+    mov py, #0
+    mov count, #50
 
-.gfx_t2_lp
-    mov r0, #255    ; cx
-    mov tmp, r4
+.loop
+    mov r0, #.centre_x
+    mov tmp, px
     asr tmp, #5
     mov r2, r0
-    add r2, tmp     ; arg_x2 = cx+px>>5
+    add r2, tmp     ; x2 = centre_x + px>>5
     asr tmp, #2
-    add r0, tmp     ; arg_x1 = cx+px>>7
+    add r0, tmp     ; x1 = centre_x + px>>7
 
-    mov r1, #200    ; arg_y1
-    mov tmp, r5
+    mov r1, #.centre_y
+    mov tmp, py
     asr tmp, #5
     mov r3, r1
-    add r3, tmp     ; arg_y2
+    add r3, tmp     ; y2 = centre_y + py>>5
     asr tmp, #2
-    add r1, tmp
+    add r1, tmp     ; y1 = centre_y + py>>7
 
     bl .gfx_line
 
-    sub r6, #1
+    sub count, #1
     preq
-    bra .gfx_t2_done
+    bra .done
 
-    mov r0, r4
+    mov r0, px
     asr r0, #3
-    add r5, r0
+    add py, r0
 
-    mov r1, r5
+    mov r1, py
     asr r1, #3
-    sub r4, r1
+    sub px, r1
 
-    bra .gfx_t2_lp
+    bra .loop
 
-.gfx_t2_done
-    hlt
+.done
+    ldw r4, [sp,#2]
+    ldw r5, [sp,#4]
+    ldw r6, [sp,#6]
+    add sp, #8
+    ldw pc, [sp]
+}
 
 ;; .gfx_clear()
 ;;
 ;; Clear video memory to zero.
 ;;
-.gfx_clear
-    alias r0 ch
-    alias r1 n
-    ; r12 tmp
+.gfx_clear {
+    alias r0 zero
+    alias r1 count
 
-    mov tmp, #hi(.MEM_TOP-512*400/8)
-    add tmp, #lo(.MEM_TOP-512*400/8)
-    mov ch, #0x0000
-    mov n, #hi(512*400/8)
-    add n, #lo(512*400/8)
-.gfx_clear_loop
-    stw ch, [tmp]
-    stw ch, [tmp,#2]
-    stw ch, [tmp,#4]
-    stw ch, [tmp,#6]
-    stw ch, [tmp,#8]
-    stw ch, [tmp,#10]
-    stw ch, [tmp,#12]
-    stw ch, [tmp,#14]
+    mov tmp, #hi(.GFX_BASE)
+    add tmp, #lo(.GFX_BASE)
+    mov zero, #0x0000
+    mov count, #hi(512*400/8)
+    add count, #lo(512*400/8)
+.loop
+    stw zero, [tmp]
+    stw zero, [tmp,#2]
+    stw zero, [tmp,#4]
+    stw zero, [tmp,#6]
+    stw zero, [tmp,#8]
+    stw zero, [tmp,#10]
+    stw zero, [tmp,#12]
+    stw zero, [tmp,#14]
     add tmp, #16
-    sub n, #16
+    sub count, #16
     prne
-    bra .gfx_clear_loop
+    bra .loop
     mov pc, link
+}
 
 ;; .gfx_line(r0=x0, r1=y0, r2=x1, r3=y1, r4=mask, r5=color)
 ;;
@@ -179,7 +226,7 @@ alias r15 pc
 ;;      mask=1 color=0 => transparent
 ;;      mask=1 color=1 => invert
 ;;
-.gfx_line
+.gfx_line {
     alias r0 x
     alias r1 y      ; only used during init
     alias r2 x2     ; only used during init
@@ -232,18 +279,18 @@ alias r15 pc
     asl dx, #1          ;; dx = (x2-x)*2
 
     prpl
-    bra .gfx_line_pos_dx
+    bra .pos_dx
     mov x, x2
     mov y, y2
     rsb dx, tmp
     rsb dy, tmp
-.gfx_line_pos_dx
+.pos_dx
     cmp dy, tmp
     prpl
-    bra .gfx_line_pos_dy
+    bra .pos_dy
     rsb dy, tmp
     rsb ystep, tmp
-.gfx_line_pos_dy
+.pos_dy
     mov tmp, #1<<7
     mov mask, tmp
     lsl mask, #8
@@ -263,31 +310,32 @@ alias r15 pc
 
     cmp dx, dy
     prlo
-    bra .gfx_line_steep
+    bra .steep
 
+.shallow {
     mov count, dx
     lsr count, #1
     mov err, count
 
-.gfx_line_shallow_loop
+.loop
     ror pattern, #1
     prcc
-    bra .gfx_line_shallow_skip
+    bra .skip
     ldb tmp, [addr]
     orr tmp, mask
     stb tmp, [addr]
-.gfx_line_shallow_skip
+.skip
     sub count, #1
     prmi
-    bra .gfx_line_done
+    bra .done
 
     sub err, dy
     prpl
-    bra .gfx_line_no_ystep
+    bra .no_ystep
     add err, dx
     add addr, ystep
 
-.gfx_line_no_ystep
+.no_ystep
     add x, #1
     mov tmp, #7
     and tmp, x
@@ -295,28 +343,29 @@ alias r15 pc
     add addr, #1
     ror mask, #1
 
-    bra .gfx_line_shallow_loop
+    bra .loop
+}
 
-.gfx_line_steep
+.steep {
     mov count, dy
     lsr count, #1
     mov err, count
 
-.gfx_line_steep_loop
+.loop
     ror pattern, #1
     prcc
-    bra .gfx_line_steep_skip
+    bra .skip
     ldb tmp, [addr]
     orr tmp, mask
     stb tmp, [addr]
-.gfx_line_steep_skip
+.skip
     sub count, #1
     prmi
-    bra .gfx_line_done
+    bra .done
 
     sub err, dx
     prpl
-    bra .gfx_line_no_xstep
+    bra .no_xstep
     add err, dy
     add x, #1
     mov tmp, #7
@@ -325,11 +374,12 @@ alias r15 pc
     add addr, #1
     ror mask, #1
 
-.gfx_line_no_xstep
+.no_xstep
     add addr, ystep
-    bra .gfx_line_steep_loop
+    bra .loop
+}
 
-.gfx_line_done
+.done
     ldw r4, [sp,#2]
     ldw r5, [sp,#4]
     ldw r6, [sp,#6]
@@ -338,18 +388,16 @@ alias r15 pc
     ldw r9, [sp,#12]
     add sp, #12
     mov pc, link
+}
 
 
 ;; .video_set_mode(r0=mode)
 ;;
 ;; Set video mode 0..3
 ;;
-alias r0 mode
-alias r0 entry
-alias r1 vreg
-alias r2 count
+.video_set_mode {
+    alias r0 mode
 
-.video_set_mode
     ; validate
     mov tmp, #4
     cmp mode, tmp
@@ -357,69 +405,76 @@ alias r2 count
     mov pc, link
 
     lsl mode, #1
-    mov tmp, #hi(.video_mode_table)
-    add tmp, #lo(.video_mode_table)
+    mov tmp, #hi(.mode_ptrs)
+    add tmp, #lo(.mode_ptrs)
     add tmp, mode
+
+    alias r0 entry
+    alias r1 vreg
+    alias r2 count
 
     ldw entry, [tmp]       ; point to data
 
+.control {
     mov vreg, #hi(.VREG)
     add vreg, #lo(.VREG)
     mov count, #11          ; 11 registers
-.video_set_mode_lp
+.loop
     ldb tmp, [entry]
     stb tmp, [vreg]
     add entry, #1
     add vreg, #1
     sub count, #1
     prne
-    bra .video_set_mode_lp
+    bra .loop
+}
 
+.palette {
     ldb count, [entry]        ; number of colours
     add entry, #1
-    mov vreg, #hi(.VREG + 0x10)
+    mov vreg, #hi(.VREG + 0x10) ; red palette
     add vreg, #lo(.VREG + 0x10)
-.video_set_mode_pal_lp
+.loop
     ldb tmp, [entry]
     stb tmp, [vreg]
 
-    add vreg, #0x10
+    add vreg, #0x10         ; green palette
     ldb tmp, [entry,#1]
-    stb tmp, [vreg]
+    stb tmp, [vreg]         
 
-    add vreg, #0x10
+    add vreg, #0x10         ; blue palette
     ldb tmp, [entry,#2]
     stb tmp, [vreg]
 
     add entry, #3
-    sub vreg, #0x20-1
+    sub vreg, #0x20-1       ; back to red, but next entry
 
     sub count, #1
     prne
-    bra .video_set_mode_pal_lp
-
+    bra .loop
+}
     mov pc, link
 
-.video_mode_table
-    dw .video_mode_0
-    dw .video_mode_1
-    dw .video_mode_2
-    dw .video_mode_3
+.mode_ptrs {
+    dw .mode_0
+    dw .mode_1
+    dw .mode_2
+    dw .mode_3
 
-.video_mode_0
+.mode_0
     ; text 64x40
     dw .MEM_TOP - .TEXT_COLS * .TEXT_ROWS               ; base addr
     dw (.VGA_WIDTH  - .TEXT_COLS * 8) / 2 + .VGA_HBACK  ; vp_left
     dw (.VGA_WIDTH  + .TEXT_COLS * 8) / 2 + .VGA_HBACK  ; vp_right
     dw (.VGA_HEIGHT - .TEXT_ROWS * 8) / 2 + .VGA_VBACK  ; vp_top
     dw (.VGA_HEIGHT + .TEXT_ROWS * 8) / 2 + .VGA_VBACK  ; vp_bottom
-    db .MODE_TEXT | .HZOOM_2 | .VZOOM_1
+    db .MODE_TEXT | .HZOOM_1 | .VZOOM_1
     db 2                    ; 2 colour palette
     db 0xff, 0xaa, 0xaa     ; 0=pink
     db 0x00, 0x00, 0xcc     ; 1=dark blue
     align
 
-.video_mode_1
+.mode_1
     ; 512x400, 1-bpp graphics
     dw .MEM_TOP - 512*400/8
     dw (.VGA_WIDTH  - 512) / 2 + .VGA_HBACK  ; vp_left
@@ -432,7 +487,7 @@ alias r2 count
     db 0x22, 0xff, 0x22     ; 1=bright green
     align
 
-.video_mode_2
+.mode_2
     ; 256x400, 2-bpp graphics
     dw .MEM_TOP - 256*400/8 * 2
     dw (.VGA_WIDTH  - 256*2) / 2 + .VGA_HBACK  ; vp_left
@@ -447,7 +502,7 @@ alias r2 count
     db 0xff, 0xff, 0xff     ; 3=white
     align
 
-.video_mode_3
+.mode_3
     ; 256x200, 4-bpp graphics
     dw .MEM_TOP - 256*200/8 * 4
     dw (.VGA_WIDTH  - 256*2) / 2 + .VGA_HBACK  ; vp_left
@@ -473,43 +528,69 @@ alias r2 count
     db 0x00, 0xff, 0xff     ; 14=cyan
     db 0xff, 0xff, 0xff     ; 15=white
     align
+}
+}
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ROUTINE .text_card_table()
-.text_card_table
+.text_card_table {
+    alias r0 ch     ; as used by text_draw
+    alias r1 x      ; as used by text_draw
+    alias r2 y      ; as used by text_draw
+    alias r3 dx     ; as used by text_draw
+    alias r4 dy     ; as used by text_draw
+    alias r5 count  ; as used by text_draw
+    alias r6 entry
+
     stw link, [sp]
-    sub sp, #2
-    mov tmp, #hi(.text_card_table_data)
-    add tmp, #lo(.text_card_table_data)
-.text_card_table_lp
-    ldb ch, [tmp, #0]
+    sub sp, #8
+    stw r4, [sp,#2]
+    stw r5, [sp,#4]
+    stw r6, [sp,#6]
+
+    mov entry, #hi(.text_card_data)
+    add entry, #lo(.text_card_data)
+
+.loop
+    ldb ch, [entry, #0]
     orr ch, ch
     preq
-    bra .text_card_table_exit
-    ldb n,  [tmp, #1]
-    ldb x,  [tmp, #2]
-    ldb y,  [tmp, #3]
-    ldb dx, [tmp, #4]
-    ldb dy, [tmp, #5]
-    add tmp, #6
+    bra .done
+    ldb x,    [entry, #1]
+    ldb y,    [entry, #2]
+    ldb dx,   [entry, #3]
+    ldb dy,   [entry, #4]
+    ldb count,[entry, #5]
+    add entry, #6
     bl .text_draw
-    bra .text_card_table_lp
-.text_card_table_exit
-    add sp, #2
+    bra .loop
+
+.done
+    ldw r4, [sp,#2]
+    ldw r5, [sp,#4]
+    ldw r6, [sp,#6]
+    add sp, #8
     ldw pc, [sp]
+}
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ROUTINE text_card_rows()
 
-.text_card_rows
+.text_card_rows {
+    alias r0 ch     ; as used by text_char_at
+    alias r1 x      ; as used by text_char_at
+    alias r2 y      ; as used by text_char_at
+
     stw link, [sp]
     sub sp, #2
 
     mov x, #8
     mov y, #0
     mov ch, #'0'
-.text_card_rows_lp
+
+.loop
     bl .text_char_at
+
     add ch, #1
     mov tmp, #'9'+1
     cmp ch, tmp
@@ -519,23 +600,31 @@ alias r2 count
     mov tmp, #.TEXT_ROWS
     cmp y, tmp
     prne
-    bra .text_card_rows_lp
-.text_card_rows_exit
+    bra .loop
+
+.done
     add sp, #2
     ldw pc, [sp]
+}
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ROUTINE text_card_cols()
 
-.text_card_cols
+.text_card_cols {
+    alias r0 ch     ; as used by text_char_at
+    alias r1 x      ; as used by text_char_at
+    alias r2 y      ; as used by text_char_at
+
     stw link, [sp]
     sub sp, #2
 
     mov x, #0
     mov y, #8
     mov ch, #'0'
-.text_card_cols_lp
+
+.loop
     bl .text_char_at
+
     add ch, #1
     mov tmp, #'9'+1
     cmp ch, tmp
@@ -545,21 +634,28 @@ alias r2 count
     mov tmp, #.TEXT_COLS
     cmp x, tmp
     prne
-    bra .text_card_cols_lp
-.text_card_cols_exit
+    bra .loop
+
+.done
     add sp, #2
     ldw pc, [sp]
+}
 
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ROUTINE text_card_ascii()
-.text_card_ascii
+.text_card_ascii {
+    alias r0 ch     ; as used by text_char_at
+    alias r1 x      ; as used by text_char_at
+    alias r2 y      ; as used by text_char_at
+
     stw link, [sp]
     sub sp, #2
+
     mov ch, #0
 
-.text_card_ascii_lp
+.loop
     mov tmp, #0x0f
     mov x, ch
     and x, tmp
@@ -569,29 +665,36 @@ alias r2 count
     lsr y, #4
     add y, #(.TEXT_ROWS-16) / 2
 
+    mov r0, ch
+    mov r1, x
+    mov r2, y
     bl .text_char_at
+
     add ch, #1
     tst ch, #bit 8
     preq
-    bra .text_card_ascii_lp
+    bra .loop
 
-.text_card_ascii_exit
+.done
     add sp, #2
     ldw pc, [sp]
+}
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ROUTINE .text_clear(ch)
-.text_clear
-    stw tmp, [sp]
-    sub sp, #2
+;; ROUTINE .text_clear(r0=ch)
+.text_clear {
+    alias r0 ch
+    alias r1 count
 
     mov tmp, ch
     lsl ch, #8
     orr ch, tmp
-    mov tmp, video
-    mov n, #hi(.TEXT_ROWS*.TEXT_COLS/16)
-    add n, #lo(.TEXT_ROWS*.TEXT_COLS/16)
-.text_clear_lp
+    mov tmp, #hi(.TEXT_BASE)
+    add tmp, #lo(.TEXT_BASE)
+    mov count, #hi(.TEXT_ROWS*.TEXT_COLS/16)
+    add count, #lo(.TEXT_ROWS*.TEXT_COLS/16)
+
+.loop
     stw ch, [tmp,#0]
     stw ch, [tmp,#2]
     stw ch, [tmp,#4]
@@ -601,46 +704,72 @@ alias r2 count
     stw ch, [tmp,#12]
     stw ch, [tmp,#14]
     add tmp, #16
-    sub n, #1
+    sub count, #1
     prne
-    bra .text_clear_lp
-.text_clear_exit
-    add sp, #2
-    ldw tmp, [sp]
+    bra .loop
+
+.done
     mov pc, link
+}
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ROUTINE .text_draw(ch, x, y, dx, dy, n)
-.text_draw
+;; ROUTINE .text_draw(ch, x, y, dx, dy, count)
+.text_draw {
+    alias r0 ch     ; as used by text_char_at
+    alias r1 x      ; as used by text_char_at
+    alias r2 y      ; as used by text_char_at
+    alias r3 dx
+    alias r4 dy
+    alias r5 count
+
     stw link, [sp]
-    sub sp, #2
-.text_draw_lp
+    sub sp, #6
+    stw r4, [sp,#2]
+    stw r5, [sp,#4]
+
+.loop
     bl .text_char_at
-    sub n, #1
+
+    sub count, #1
     preq
-    bra .text_draw_exit
+    bra .done
     add x, dx
     add y, dy
-    bra .text_draw_lp
-.text_draw_exit
-    add sp, #2
+    bra .loop
+
+.done
+    ldw r4, [sp,#2]
+    ldw r5, [sp,#4]
+    add sp, #6
     ldw pc, [sp]
+}
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ROUTINE .text_char_at(ch, x, y)
-.text_char_at
-    stw tmp, [sp]
+;;
+;; Guarantees not to corrupt ch, x, y.
+;;
+.text_char_at {
+    alias r0 ch
+    alias r1 x
+    alias r2 y
+    alias r4 video
+
     sub sp, #2
+    stw video, [sp,#2]
+
+    mov video, #hi(.TEXT_BASE)
+    add video, #lo(.TEXT_BASE)
 
     ; sanity check (x,y)
     mov tmp, #.TEXT_ROWS
     cmp y, tmp
     prhs
-    bra .text_char_at_exit
+    bra .done
     mov tmp, #.TEXT_COLS
     cmp x, tmp
     prhs
-    bra .text_char_at_exit
+    bra .done
 
     mov tmp, y
     lsl tmp, #6
@@ -648,68 +777,69 @@ alias r2 count
     add tmp, video
     stb ch, [tmp]
 
-.text_char_at_exit
+.done
+    ldw video, [sp,#2]
     add sp, #2
-    ldw tmp, [sp]
     mov pc, link
+}
 
-.text_card_table_data
+.text_card_data
     ; vertical lines, first col
-    ;  ch    n  x  y   dx dy
-    db 0x80, 4, 0, 4,  0, 1
-    db 0x81, 4, 0, 8,  0, 1
-    db 0x82, 4, 0, 12, 0, 1
-    db 0x83, 4, 0, 16, 0, 1
-    db 0x84, 4, 0, 20, 0, 1
-    db 0x85, 4, 0, 24, 0, 1
-    db 0x86, 4, 0, 28, 0, 1
-    db 0x87, 4, 0, 32, 0, 1
-    db 0xa2, 1, 2, 4,  0, 1
-    db 0xa2, 1, 2, 35, 0, 1
+    ;  ch    x  y  dx dy  n
+    db 0x80, 0, 4,  0, 1, 4
+    db 0x81, 0, 8,  0, 1, 4
+    db 0x82, 0, 12, 0, 1, 4
+    db 0x83, 0, 16, 0, 1, 4
+    db 0x84, 0, 20, 0, 1, 4
+    db 0x85, 0, 24, 0, 1, 4
+    db 0x86, 0, 28, 0, 1, 4
+    db 0x87, 0, 32, 0, 1, 4
+    db 0xa2, 2, 4,  0, 1, 1
+    db 0xa2, 2, 35, 0, 1, 1
 
     ; vertical lines, last col
-    ;  ch    n  x  y   dx dy
-    db 0x80, 4, 63, 4,  0, 1
-    db 0x81, 4, 63, 8,  0, 1
-    db 0x82, 4, 63, 12, 0, 1
-    db 0x83, 4, 63, 16, 0, 1
-    db 0x84, 4, 63, 20, 0, 1
-    db 0x85, 4, 63, 24, 0, 1
-    db 0x86, 4, 63, 28, 0, 1
-    db 0x87, 4, 63, 32, 0, 1
-    db 0xa3, 1, 61, 4,  0, 1
-    db 0xa3, 1, 61, 35, 0, 1
+    ;  ch    x  y   dx dy  n
+    db 0x80, 63, 4,  0, 1, 4
+    db 0x81, 63, 8,  0, 1, 4
+    db 0x82, 63, 12, 0, 1, 4
+    db 0x83, 63, 16, 0, 1, 4
+    db 0x84, 63, 20, 0, 1, 4
+    db 0x85, 63, 24, 0, 1, 4
+    db 0x86, 63, 28, 0, 1, 4
+    db 0x87, 63, 32, 0, 1, 4
+    db 0xa3, 61, 4,  0, 1, 1
+    db 0xa3, 61, 35, 0, 1, 1
 
     ; horiz lines, first row
-    ;  ch    n  x   y   dx dy
-    db 0x88, 7, 4,  0,  1, 0
-    db 0x89, 7, 11, 0,  1, 0
-    db 0x8a, 7, 18, 0,  1, 0
-    db 0x8b, 7, 25, 0,  1, 0
-    db 0x8c, 7, 32, 0,  1, 0
-    db 0x8d, 7, 39, 0,  1, 0
-    db 0x8e, 7, 46, 0,  1, 0
-    db 0x8f, 7, 53, 0,  1, 0
-    db 0xa0, 1, 4,  2,  1, 0
-    db 0xa0, 1, 59, 2,  1, 0
+    ;  ch    x   y   dx dy n
+    db 0x88, 4,  0,  1, 0, 7
+    db 0x89, 11, 0,  1, 0, 7
+    db 0x8a, 18, 0,  1, 0, 7
+    db 0x8b, 25, 0,  1, 0, 7
+    db 0x8c, 32, 0,  1, 0, 7
+    db 0x8d, 39, 0,  1, 0, 7
+    db 0x8e, 46, 0,  1, 0, 7
+    db 0x8f, 53, 0,  1, 0, 7
+    db 0xa0, 4,  2,  1, 0, 1
+    db 0xa0, 59, 2,  1, 0, 1
 
     ; horiz lines, last row
-    ;  ch    n  x   y   dx dy
-    db 0x88, 7, 4,  39, 1, 0
-    db 0x89, 7, 11, 39, 1, 0
-    db 0x8a, 7, 18, 39, 1, 0
-    db 0x8b, 7, 25, 39, 1, 0
-    db 0x8c, 7, 32, 39, 1, 0
-    db 0x8d, 7, 39, 39, 1, 0
-    db 0x8e, 7, 46, 39, 1, 0
-    db 0x8f, 7, 53, 39, 1, 0
-    db 0xa1, 1, 4,  37, 1, 0
-    db 0xa1, 1, 59, 37, 1, 0
+    ;  ch    x   y  dx dy  n
+    db 0x88, 4,  39, 1, 0, 7
+    db 0x89, 11, 39, 1, 0, 7
+    db 0x8a, 18, 39, 1, 0, 7
+    db 0x8b, 25, 39, 1, 0, 7
+    db 0x8c, 32, 39, 1, 0, 7
+    db 0x8d, 39, 39, 1, 0, 7
+    db 0x8e, 46, 39, 1, 0, 7
+    db 0x8f, 53, 39, 1, 0, 7
+    db 0xa1, 4,  37, 1, 0, 1
+    db 0xa1, 59, 37, 1, 0, 1
 
     ; corners
-    db 0xa4, 1, 0,  0,  1, 0
-    db 0xa5, 1, 63, 0,  1, 0
-    db 0xa6, 1, 0,  39, 1, 0
-    db 0xa7, 1, 63, 39, 1, 0
+    db 0xa4, 0,  0,  1, 0, 1
+    db 0xa5, 63, 0,  1, 0, 1
+    db 0xa6, 0,  39, 1, 0, 1
+    db 0xa7, 63, 39, 1, 0, 1
 
     db 0x00
