7f0b017f6450a4cd05a82c04a343e6caf53cd785
Alasdair McIntyre <alasdair.mcintyre@gmail.com>
2023-12-21 00:42:35 +0000

    integer/float conversions

---
+++ programs/f16/ftoi.asm
@@ -0,0 +1,192 @@
+def VALUE 0xbc00
+
+mov r0, #hi(.VALUE)
+add r0, #lo(.VALUE)
+bl .f16_ftou
+hlt
+
+def f16_info_negative   0x8000
+def f16_info_non_finite 0x4000
+def f16_info_nan        0x2000
+
+; Convert float to 16-bit unsigned integer, truncating towards zero.
+;
+; On exit:
+;   if V is clear:
+;       num=integer part of a
+;   if V is set:
+;       num=0x0000 if a was NaN or < 0.0 or -inf
+;       num=0xffff if a was +inf
+;
+.f16_ftou {
+    alias r0 a
+    alias r1 info
+    alias r2 num
+
+    alias r13 sp
+    alias r14 link
+    alias r15 pc
+
+    stw link, [sp]
+    bl .f16_ftomag
+
+    and info, info
+    preq
+    ldw pc, [sp]
+
+    mov num, #0
+
+    tst info, #.f16_info_nan
+    prne
+    bra .set_overflow
+
+    tst info, #.f16_info_negative
+    preq
+    sub num, #1
+
+.set_overflow
+    mov info, #0x8000
+    add info, info
+    ldw pc, [sp]
+}
+
+
+; Convert float to 16-bit signed integer, truncating towards zero.
+;
+; On exit:
+;   if V is clear:
+;       num=integer part of a
+;
+;   if V is set:
+;       num=0      if a was NaN
+;       num=0x7fff if a was >= 32768.0 (or +inf)
+;       num=0x8000 if a was <= 32769.0 (or -inf)
+;
+.f16_ftoi {
+    alias r0 a
+    alias r1 info
+    alias r2 num
+
+    alias r13 sp
+    alias r14 link
+    alias r15 pc
+
+    stw link, [sp]
+    bl .f16_ftomag
+
+    tst info, #.f16_info_non_finite
+    prne
+    bra .non_finite
+
+    tst num, #0x8000
+    prne
+    bra .maybe_overflow
+
+    asr info, #15
+    eor num, info
+    sub num, info   ; will clear V
+    ldw pc, [sp]
+
+.non_finite
+    tst info, #.f16_info_nan
+    prne
+    bra .nan
+
+    bic info, #.f16_info_non_finite
+    mov num, #0xf000
+
+.maybe_overflow
+    tst info, #.f16_info_negative
+    preq
+    bra .pos_overflow
+    cmp num, info
+    preq            ; V will be clear from cmp
+    ldw pc, [sp]
+
+    ; return 0x8000 and set overflow
+    mov num, #0
+    sub num, info   ; info is still 0x8000 here
+    ldw pc, [sp]
+
+.pos_overflow
+    ; return 0x7fff and set overflow
+    mov num, #0x8000
+    sub num, #1
+    ldw pc, [sp]
+
+.nan
+    ; return 0 and set overflow
+    mov num, #0x8000
+    add num, num
+    ldw pc, [sp]
+}
+
+
+; Convert float to 16-bit integer magnitude (truncating towards zero),
+; with separate sign/inf/nan indicators.
+;
+; On exit:
+;   num=magnitude of integer part of a
+;   info & f16_info_negative   : a was negative
+;   info & f16_info_non_finite : a was +-inf or NaN
+;   info & f16_info_nan        : a was NaN
+;
+.f16_ftomag {
+    alias r0 a
+    alias r1 info
+    alias r2 num
+    alias r3 exp
+
+    alias r12 tmp
+    alias r14 link
+    alias r15 pc
+
+    mov info, #.f16_sign_mask
+    and info, a
+    bic a, #.f16_sign_mask
+
+    mov tmp, #.f16_one
+    cmp a, tmp
+    prlo
+    bra .zero
+
+    mov tmp, #.f16_exp_mask
+    cmp a, tmp
+    prhs
+    bra .non_finite
+
+    mov exp, a
+    lsr exp, #10
+
+    bic a, tmp
+    orr a, #bit 10
+
+    mov num, a
+
+    mov tmp, #25
+    cmp exp, tmp
+    prhi
+    bra .big
+
+    sub tmp, exp
+    lsr num, tmp
+    mov pc, link
+
+.big
+    rsb tmp, exp
+    lsl num, tmp
+    mov pc, link
+
+.zero
+    mov num, #0
+    mov info, #0
+    mov pc, link
+
+.non_finite
+    orr info, #.f16_info_non_finite
+    mov num, #0
+    cmp a, tmp
+    prne
+    orr info, #.f16_info_nan
+    mov pc, link
+}
---
+++ programs/f16/itof.asm
@@ -0,0 +1,50 @@
+; Convert 16-bit signed integer to float.
+;
+.f16_itof {
+    alias r0  num
+    alias r2  z
+    alias r3  z_exp
+    alias r4  z_sign
+
+    alias r12 tmp
+    alias r14 link
+    alias r15 pc
+
+    mov z, num
+    and z, z
+    preq
+    mov pc, link
+
+    mov tmp, #0
+    mov z_sign, #.f16_sign_mask
+    and z_sign, z
+    prmi
+    rsb z, tmp
+
+    mov z_exp, #30-1    ; account for leading 1 bit
+    clz tmp, z
+    sub z_exp, tmp
+    lsl z, tmp
+
+    lsr z, #1
+    prcs
+    orr z, #bit 0
+    bra .f16_round_pack
+}
+
+; Convert 16-bit unsigned integer to float.
+;
+.f16_utof {
+    alias r0  num
+    alias r2  z
+    alias r3  z_exp
+    alias r4  z_sign
+
+    alias r12 tmp
+    alias r14 link
+    alias r15 pc
+
+    ;
+    ; TODO
+    ;
+}
